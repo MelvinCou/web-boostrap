00005c5f96047c6dd32354ca94ce388ffcdd1f41
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-03-09 17:31:49 +0000

    Add basic ci

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,26 +12,26 @@ import (
 	swaggerFiles "github.com/swaggo/files"
 )
 
-// @title           Swagger Example API
-// @version         1.0
-// @description     This is a sample server celler server.
-// @termsOfService  http://swagger.io/terms/
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server celler server.
+//	@termsOfService	http://swagger.io/terms/
 
-// @contact.name   API Support
-// @contact.url    http://www.swagger.io/support
-// @contact.email  support@swagger.io
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
 
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8080
-// @BasePath  /api/v1
+//	@host		localhost:8080
+//	@BasePath	/api/v1
 
-// @securityDefinitions.basic  BasicAuth
+//	@securityDefinitions.basic	BasicAuth
 
-// @externalDocs.description  OpenAPI
-// @externalDocs.url          https://swagger.io/resources/open-api/
-// @schemes http https
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
+//	@schemes					http https
 func main() {
 	r := gin.Default()
 	docs.SwaggerInfo.BasePath = "/"
@@ -51,37 +52,41 @@ func main() {
 	}
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.Run() // listen and serve on 0.0.0.0:8080
+	if err := r.Run(); err != nil { // listen and serve on 0.0.0.0:8080
+		fmt.Printf("%+v\n", err)
+	}
 }
 
 // ShowAccount godoc
-// @Summary      Show an account
-// @Description  get string by ID
-// @Tags         accounts
-// @Accept       json
-// @Produce      json
-// @Param        id   path      int  true  "Account ID"
-// @Success      200  {object}  Account
-// @Failure      400  {string}	string    "error"
-// @Failure      404  {string}	string    "error"
-// @Failure      500  {string}	string    "error"
-// @Router       /accounts/{id} [get]
+//
+//	@Summary		Show an account
+//	@Description	get string by ID
+//	@Tags			accounts
+//	@Accept			json
+//	@Produce		json
+//	@Param			id	path		int	true	"Account ID"
+//	@Success		200	{object}	Account
+//	@Failure		400	{string}	string	"error"
+//	@Failure		404	{string}	string	"error"
+//	@Failure		500	{string}	string	"error"
+//	@Router			/accounts/{id} [get]
 func ShowAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, nil)
 }
 
 // ListAccounts godoc
-// @Summary      List accounts
-// @Description  get accounts
-// @Tags         accounts
-// @Accept       json
-// @Produce      json
-// @Param        q    query     string  false  "name search by q"  Format(email)
-// @Success      200  {array}   Account
-// @Failure      400  {string}	string    "error"
-// @Failure      404  {string}	string    "error"
-// @Failure      500  {string}	string    "error"
-// @Router       /accounts [get]
+//
+//	@Summary		List accounts
+//	@Description	get accounts
+//	@Tags			accounts
+//	@Accept			json
+//	@Produce		json
+//	@Param			q	query		string	false	"name search by q"	Format(email)
+//	@Success		200	{array}		Account
+//	@Failure		400	{string}	string	"error"
+//	@Failure		404	{string}	string	"error"
+//	@Failure		500	{string}	string	"error"
+//	@Router			/accounts [get]
 func ListAccounts(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, nil)
 }
